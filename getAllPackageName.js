a66d3540a01f79531490c8534e8dd88e91cc98f4
doami2005 <54644531+doami2005@users.noreply.github.com>
2020-07-17 18:13:13 +0900

    Update getAllPackageName.js

--- getAllPackageName.js
+++ getAllPackageName.js
@@ -1,3 +1,14 @@
+/*
+
+Made by doami2005
+설치된 패키지명과 앱의 이름을 가져오는 함수입니다.
+메신저봇R 전용입니다.
+
+https://open.kakao.com/o/gXaUzu1b
+질문은 여기로
+
+*/
+
 function getAllPackageName() {
     let pkgs = [];
     let pack = Api.getContext().getPackageManager().getInstalledPackages(0);
