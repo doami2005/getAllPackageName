331bbfa1847424fd3549fcc72779db3fe3e002da
doami2005 <54644531+doami2005@users.noreply.github.com>
2020-07-17 18:15:07 +0900

    Update getAllPackageName.js

--- getAllPackageName.js
+++ getAllPackageName.js
@@ -4,6 +4,10 @@ Made by doami2005
 설치된 패키지명과 앱의 이름을 가져오는 함수입니다.
 메신저봇R 전용입니다.
 
+예시 )
+getAllPackageName() // 설치된 모든 패키지명 반환
+getAppName(패키지명) // 해당 패키지명을 가진 앱 이름 반환
+
 https://open.kakao.com/o/gXaUzu1b
 질문은 여기로
 
